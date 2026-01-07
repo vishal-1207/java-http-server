3481f5a7545a7eac831249dd1dabaf663b4c54b3
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2026-01-07 23:10:41 +0530

    update variables name

--- src/com/httpServer/middleware/TimingMiddleware.java
+++ src/com/httpServer/middleware/TimingMiddleware.java
@@ -3,19 +3,19 @@ package com.httpServer.middleware;
 import com.httpServer.http.HttpRequest;
 
 public class TimingMiddleware implements Middleware {
-	private final String START = "_startTime";
+	private static final String START_TIME_KEY = "_startTime";
 
 	@Override
 	public void before(HttpRequest request) {
-		request.getAttributes().put(START, System.nanoTime());
+		request.getAttributes().put(START_TIME_KEY, System.nanoTime());
 	}
 
 	@Override
 	public void after(HttpRequest request) {
-		Object v = request.getAttributes().get(START);
+		Object start = request.getAttributes().get(START_TIME_KEY);
 		
-		if (v instanceof Long) {
-			long tookNs = System.nanoTime() - (Long) v;
+		if (start instanceof Long) {
+			long tookNs = System.nanoTime() - (Long) start;
 			double ms = tookNs / 1_000_000.0;
 			System.out.printf("Request %s %s took %.3f ms%n", request.getMethod(), request.getRawPath(), ms);
 		}
